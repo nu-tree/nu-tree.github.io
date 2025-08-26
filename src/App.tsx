cbbedf1f3bdadb690ba7ffee858d448dd061a1d7
knu9910 <knu9910@naver.com>
2025-08-26 10:26:57 +0900

    vite + tailwind + react-ts init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div className="">
+      <Button>hasdasdassi</Button>
+      didididi야야야
+    </div>
+  );
+}
+
+export default App;
